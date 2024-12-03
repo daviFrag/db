f54b4d12f4d52b7b071d1fc142a9727fc2a79f53
Davide Frageri <davide.frageri@studenti.unitn.it>
2024-12-03 10:06:40 +0100

    convert select to namedQuery

--- operations.go
+++ operations.go
@@ -2,14 +2,18 @@ package db
 
 import "log"
 
-func (dbPlugin *DbPlugin) Select(query string, inputParams []interface{}) interface{} {
-	var result interface{}
-	err := dbPlugin.db.Select(result, query, inputParams)
+func (dbPlugin *DbPlugin) Select(query string, inputParams map[string]interface{}) interface{} {
+	rows, err := dbPlugin.db.NamedQuery(query, inputParams)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	counter := 0
+	for rows.Next() {
+		log.Println(counter)
+		counter += 1
+	}
 
-	log.Println(result)
+	log.Println(rows)
 
-	return result
+	return rows
 }
